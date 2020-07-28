97ab79a6b42afe78c06394c561a1b60ea2e2add8
Christoph <christoph.k.m.amort@gmail.com>
2020-07-28 10:19:52 +0200

    added index.html view

--- app/shop/views.py
+++ app/shop/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'shop/index.html'
+
+    def get(self, request):
+        context = {}
+        return render(request, self.template_name, context)
